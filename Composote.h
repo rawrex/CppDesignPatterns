3ac8a79560e3d8fac4f48dc4b2e838e67fbe5994
rawrex <rawrex@protonmail.ch>
2022-04-18 18:18:57 +0700

    Stripped down to basic implementation (raw pointers and etc.)

--- Composote.h
+++ Composote.h
@@ -1,13 +1,20 @@
 #include <memory>
 
+class Composite;
+
 class Component {
-	std::shared_ptr<Component> parent;
+	Component * parent;
+	Component * child;
 public:
 	Component() = default;
+	virtual Composite * GetComposite() { return nullptr; }
+
 };
 
 class Composite: public Component {
-		
+public:
+	void add() {}
+	virtual Composite * GetComposite() override { return this; }
 };
 
 class Leaf: public Component {
