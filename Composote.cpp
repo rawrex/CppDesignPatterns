c76b38d57d551dfb605c5bb584d5ef83c096b872
rawrex <rawrex@protonmail.ch>
2022-04-19 16:47:35 +0700

    Cleaned up the Composite class

--- Composote.cpp
+++ Composote.cpp
@@ -3,7 +3,8 @@
 class Composite;
 
 class Component {
-	Component * parent, child;
+	Component * parent;
+	Component * child;
 public:
 	Component() = default;
 	virtual Composite * GetComposite() { return nullptr; }
@@ -13,11 +14,29 @@ public:
 class Composite: public Component {
 public:
 	void add(Composite * ptr) {}
-	virtual Composite GetComposite() override { return *this; }
+	virtual Composite * GetComposite() override { return this; }
 };
 
 class Leaf: public Component {
 
 };
 
-int main() {}
+int main() {
+
+	Composite* composite = new Composite;
+	Leaf* leaf = new Leaf;
+
+	Component* component;
+	Composite* test;
+
+	component = composite;
+
+	if (test = component->GetComposite())
+		test->add(new Leaf);
+
+	component = leaf;
+
+	if (test = component->GetComposite())
+		test->add(new Leaf);	// Won't add leaf
+
+}
