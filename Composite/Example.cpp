e26f05cd21379fc0f3df0630608e9ab222cb856c
rawrex <rawrex@protonmail.ch>
2022-04-21 15:58:32 +0700

    Cleaned up and corrected the Composite example. Compiles.

--- Composite/Example.cpp
+++ Composite/Example.cpp
@@ -1,7 +1,10 @@
 // Equipment such as computers and stereo components are often organized into
 // part-whole or containment hierarchies
-
+#include <list>
 #include <string>
+
+using Watt = long double;
+using Currency = unsigned long long;
 
 class Equipment {
 public:
@@ -18,22 +21,42 @@ public:
 	// Pattern management funcitons
 	virtual void add(Equipment*);
 	virtual void remove(Equipment*);
-	virtual Iterator<Equipment*>* createlterator();
+	//virtual Iterator<Equipment*>* createlterator();
 
 protected:
-	Equipment(const std::string & str_name) : name(str_name) {}
+	Equipment(const std::string & str_name) : name_(str_name) {}
 
 private:
 	std::string name_;
 
 };
 
+
+// A Leaf subclass
 class FloppyDisk : public Equipment {
 public:
-	FloppyDisk(cosnt std::string & str_name);
+	FloppyDisk(const std::string & str_name);
 	virtual ~FloppyDisk();
 	
 	virtual Watt power();
 	virtual Currency netPrice();
 	virtual Currency discountPrice();
 };
+
+
+class CompositeEquipment : public Equipment {
+public:
+	virtual ~CompositeEquipment();
+	virtual Watt Power();
+	virtual Currency NetPrice();
+	virtual Currency DiscountPrice();
+	virtual void Add(Equipment*);
+	virtual void Remove(Equipment*);
+	//virtual Iterator<Equipment*>* Createlterator();
+protected:
+	CompositeEquipment(const char*);
+private:
+	std::list<Equipment*> _equipment;
+};
+
+int main () {}
