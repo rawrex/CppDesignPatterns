88b36b78ec8ed7ce1db150ea26823305cbdeffce
rawrex <rawrex@protonmail.ch>
2022-04-28 14:50:31 +0700

    Strategy pattern initialization.

--- Composite/Example.cpp
+++ Composite/Example.cpp
@@ -77,4 +77,22 @@ Currency CompositeEquipment::NetPrice () {
 	return total;
 }
 
-int main () {}
+class Chassis : public CompositeEquipment {
+public:
+	Chassis(const std::string&);
+	virtual "Chassis();
+	virtual Watt Power();
+	virtual Currency NetPrice();
+	virtual Currency DiscountPrice();
+};
+
+int main () {
+	Cabinet* cabinet = new Cabinet("PC Cabinet");
+	Chassis* chassis = new Chassis("PC Chassis");
+	cabinet->Add(chassis);
+	Bus* bus = new Bus("MCA Bus");
+	bus->Add(new Card("16Mbs Token Ring"));
+	chassis->Add(bus);
+	chassis->Add(new FloppyDisk("3.Bin Floppy"));
+	std::cout << "The net price is " << chassis->NetPrice() « std::endl;
+}
