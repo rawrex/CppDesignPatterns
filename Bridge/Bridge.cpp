47783ab6e8e8b0c62edbc38c45833fa5ac444d14
rawrex <rawrex@proton.me>
2022-06-15 23:54:12 +0700

    Added some clarifying comments to the Bridge pattern as well as more proper class structure.

--- Bridge/Bridge.cpp
+++ Bridge/Bridge.cpp
@@ -1,21 +1,22 @@
 #include <memory>
 #include <iostream>
 
-// https://refactoring.guru/design-patterns/bridge
-//
 // In Bridge we use object composition for implementations diversity
 
 
-class AbstractImplementor 
-{
-	// Data members...
+// Implementation part
+//
+// This is a class that provides abstracted from any particular implementation 
+// set of operations to which a user of the system class can appeal via the Interface (Abstraction) 
+
+class AbstractImplementor {
+	// ...
 public:
 	virtual void imp_operation() = 0;
 };
 
-class ConcreteImplementor_A: public AbstractImplementor
-{
-	
+class ConcreteImplementor_A: public AbstractImplementor {
+	// ...
 public:
 	void imp_operation() override 
 	{
@@ -23,8 +24,9 @@ public:
 	}	
 };
 
-class ConcreteImplementor_B: public AbstractImplementor
-{
+class ConcreteImplementor_B: public AbstractImplementor {
+	// ...
+public:
 	void imp_operation() override 
 	{
 		// Do the real implementation specific work
@@ -32,18 +34,34 @@ class ConcreteImplementor_B: public AbstractImplementor
 };
 
 
+// Interface part
+// 
+// An abstracted high-level, user-accessible, controller class
+// Which relies on the intentionnaly separate class hierarchy of Implementors to the job requested
+// An implementor in turn builds upon the primitives provided by a particular implementation.
+
 class Abstraction
 {
 	std::shared_ptr<AbstractImplementor> implementation;	
-	// Other data members...
+	// ...
 
 public:
 	virtual void operation()
 	{
 		// ...
 		implementation->imp_operation();
-		// ...
 	}
 };
+
+// These two add up to the class hierarchy of interfaces
+class RefinedAbstraction_A: public Abstraction 
+{
+	// Extended interface for the Abstraction
+}
+
+class RefinedAbstraction_B: public Abstraction 
+{
+	// Extended interface for the Abstraction
+}
 
 int main() {}
