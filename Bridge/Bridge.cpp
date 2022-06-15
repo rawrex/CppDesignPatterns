4165226362ffb2ec40092e3244d90f8112703902
rawrex <rawrex@proton.me>
2022-06-15 22:49:14 +0700

    Added basis for the abstract Bridge demo implementaiton.

--- Bridge/Bridge.cpp
+++ Bridge/Bridge.cpp
@@ -1,1 +1,49 @@
+#include <memory>
+#include <iostream>
+
 // https://refactoring.guru/design-patterns/bridge
+//
+// In Bridge we use object composition for implementations diversity
+
+
+class AbstractImplementor 
+{
+	// Data members...
+public:
+	virtual void imp_operation() = 0;
+};
+
+class ConcreteImplementor_A: public AbstractImplementor
+{
+	
+public:
+	void imp_operation() override 
+	{
+		// Do the real implementation specific work
+	}	
+};
+
+class ConcreteImplementor_B: public AbstractImplementor
+{
+	void imp_operation() override 
+	{
+		// Do the real implementation specific work
+	}	
+};
+
+
+class Abstraction
+{
+	std::shared_ptr<AbstractImplementor> implementation;	
+	// Other data members...
+
+public:
+	virtual void operation()
+	{
+		// ...
+		implementation->imp_operation();
+		// ...
+	}
+};
+
+int main() {}
