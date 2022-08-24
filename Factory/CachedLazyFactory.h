e56e017c68a3c9a2304e3fe76fd55e374db5a2d2
rawrex <rawrex@proton.me>
2022-08-24 12:38:24 +0700

    More work on the CachedFactory was done. Met the problem in the fact that it is not straightfowrward to have two users of a unique_ptr (no surprise).

--- Factory/CachedLazyFactory.h
+++ Factory/CachedLazyFactory.h
@@ -4,22 +4,30 @@
 class CachedLazyFactory: public BasicFactory
 {
 protected:
-	std::shared_ptr<Product> createFooProduct() const
-	{
-		return std::make_shared<FooProduct>();
-	}
+	virtual Product* createProduct() const = 0;
 
 public:
-	CachedLazyFactory() = default;
-	~CachedLazyFactory() override = default;	
-
-	std::shared_ptr<Product> getProduct()
+	std::unique_ptr<Product> Create() const override
 	{
 		if(!product)
-			product = createFooProduct();
-		return product;
+			product = createProduct();
+		return std::unique_ptr<Product>(product);
 	}
 
+	CachedLazyFactory() = default;
+	virtual ~CachedLazyFactory() override = default;	
 private:
-	std::shared_ptr<Product> product;
+	mutable Product* product;
+};
+
+
+class CachedLazyFooFactory: public CachedLazyFactory
+{
+private:
+	virtual Product* createProduct() const override
+	{
+		return new FooProduct;
+	}
+public:
+	// virtual ~CachedLazyFooFactory() override = default;
 };
--- Factory/main.cpp
+++ Factory/main.cpp
@@ -4,11 +4,15 @@
 int main() {
 
 	// We use the same creator object to create different types of products
-	FooFactory foo_creator;
+	CachedLazyFooFactory foo_creator;
 	BarFactory bar_creator;
 	BazFactory baz_creator;
 
 	Client client;
+	client.use(foo_creator);
+	client.use(bar_creator);
+	client.use(baz_creator);
+
 	client.use(foo_creator);
 	client.use(bar_creator);
 	client.use(baz_creator);
