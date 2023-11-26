07d64ee1df3ecb392de8f2ab274274c7a7596be4
rawrex <rawrex@proton.me>
2023-11-26 21:43:18 +0700

    Changed to use the v143 platform toolset and commented out buggy code.

--- Factory/src/main.cpp
+++ Factory/src/main.cpp
@@ -1,20 +1,15 @@
 #include "BasicFactory.h"
 #include "CachedLazyFactory.h"
 
-int main() {
-
+int main() 
+{
 	// We use the same creator object to create different types of products
-	CachedLazyFooFactory foo_creator;
+	// CachedLazyFooFactory foo_creator;
 	BarFactory bar_creator;
 	BazFactory baz_creator;
 
 	Client client;
-	client.use(foo_creator);
+	// client.use(foo_creator);
 	client.use(bar_creator);
 	client.use(baz_creator);
-
-	client.use(foo_creator);
-	client.use(bar_creator);
-	client.use(baz_creator);
-
 }
